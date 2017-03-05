20d40cdb6a0a61f5a1a6edf1ff08485b4e027890
andrei.mihut95 <andrei.mihut95@DESKTOP-CIA36DG>
2017-03-05 23:51:27 +0200

    [Andrei Mihut]-println xml file

--- MovieRecommender/src/main/java/com/furiapolitehnicii/main/Main.java
+++ MovieRecommender/src/main/java/com/furiapolitehnicii/main/Main.java
@@ -1,9 +1,20 @@
 package com.furiapolitehnicii.main;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
 
 public class Main {
-	public static void main(String[] args)
+	
+	public static void main(String[] args) throws DocumentException, IOException
 	{
+		File xmlFile = new File("src/main/resources/movies.xml");
+		SAXReader reader = new SAXReader();
+		Document document = reader.read(xmlFile);
+		
+		System.out.println(document.asXML());
 	}
-
 }
